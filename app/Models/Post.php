5ef5cdf42a06344b913086c31910430803b25dbb
David-Rosas <davidrosas1987@gmail.com>
2023-10-23 16:33:52 -0500

    Added new features filter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,11 +13,22 @@ class Post extends Model
 
     protected $fillable = ['category_id', 'title', 'slug', 'excerpt', 'body'];
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function author(){
+    public function author()
+    {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filter['search'] ?? false, fn($query, $search) =>
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'));
+        ;
     }
 }
